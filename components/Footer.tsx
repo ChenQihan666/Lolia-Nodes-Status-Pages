40485debde6ee245861388ca77219e743cf56184
QihanNX <nuo66688@163.com>
2026-07-29 17:50:04 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { pageConfig } from '@/uptime.config'
 
 export default function Footer() {
   const defaultFooter =
-    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> 洛链映射节点监测平台，如有任何问题，请加群:975159586 </p>'
+    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> 洛链映射节点监测平台，如有任何问题，请加QQ群:975159586 </p>'
 
   return (
     <>
