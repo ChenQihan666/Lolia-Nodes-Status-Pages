115359a3766b0c0d210a7f4817861ad5ac4b5cf9
QihanNX <nuo66688@163.com>
2026-07-29 22:48:14 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header({ style }: { style?: React.CSSProperties }) {
       <Container size="md" className={classes.inner}>
         <div>
           <a
-            href={location.pathname == '/' ? 'https://lolianode.top/' : '/'}
+            href={location.pathname == '/' ? 'https://lolianode.top' : '/'}
             target={location.pathname == '/' ? '_blank' : undefined}
           >
             <Image
